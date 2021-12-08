2495ff0171dbde497334dea8ed8f0d377c3ebfb5
Nadir Hamid <matrix.nad@gmail.com>
2021-12-08 03:47:08 +0000

    first

---
+++ go.mod
@@ -0,0 +1,51 @@
+module github.com/nadirhamid/golang-etcd-rate-limiter
+
+go 1.17
+
+require (
+	github.com/beorn7/perks v1.0.0 // indirect
+	github.com/coreos/go-semver v0.2.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.0.0 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dustin/go-humanize v0.0.0-20171111073723-bb3d318650d4 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/golang/groupcache v0.0.0-20160516000752-02826c3e7903 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/uuid v1.0.0 // indirect
+	github.com/gorilla/websocket v0.0.0-20170926233335-4201258b820c // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.1-0.20190118093823-f849b5445de4 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.9.5 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.0.0 // indirect
+	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4 // indirect
+	github.com/prometheus/common v0.4.1 // indirect
+	github.com/prometheus/procfs v0.0.2 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/soheilhy/cmux v0.1.4 // indirect
+	github.com/spf13/cobra v0.0.3 // indirect
+	github.com/spf13/pflag v1.0.1 // indirect
+	github.com/tmc/grpc-websocket-proxy v0.0.0-20170815181823-89b8d40f7ca8 // indirect
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
+	go.etcd.io/bbolt v1.3.3 // indirect
+	go.etcd.io/etcd v0.5.0-alpha.5.0.20200306183522-221f0cc107cb // indirect
+	go.uber.org/atomic v1.3.2 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc // indirect
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
+	golang.org/x/sys v0.0.0-20190826190057-c7b8b68b1456 // indirect
+	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/time v0.0.0-20180412165947-fbb02b2291d2 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+	sigs.k8s.io/yaml v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"fmt"
+	"context"
+	"time"
+	"strconv"
+	"go.etcd.io/etcd/clientv3"
+)
+
+type RateLimiter struct {
+	Etcd *clientv3.Client
+
+	Limit         uint64
+	BaseKey       string
+	Interval      time.Duration
+	FlushInterval time.Duration
+	ticker     *time.Ticker
+	stopTicker chan bool
+	keys map[string]string
+}
+
+// New returns an instance or RateLimiter, which isn't yet initialized
+func NewRateLimiter(etcd * clientv3.Client, baseKey string, limit uint64, interval time.Duration, flushInterval time.Duration) *RateLimiter {
+	keys:=make(map[string]string)
+	keys["requests"]=baseKey+"_requests"
+	keys["time_initialized"]=baseKey+"_time_initialized"
+	keys["deadline"]=baseKey+"_deadline"
+	rl := &RateLimiter{
+		Etcd: etcd,
+		Limit:         limit,
+		BaseKey:       baseKey,
+		Interval:      interval,
+		FlushInterval: flushInterval,
+		keys: keys,
+	}
+
+	return rl
+}
+
+// Initialize the key if its not set yet
+// This is used to initialize the deadline, and request counts
+func (rl *RateLimiter) initializeKeyIfNeeded(key string, initValue string) (error) {
+	ctx := context.Background()
+    kv := clientv3.NewKV(rl.Etcd)
+    _, err := kv.Get(ctx, key)
+	if err == nil {
+		return nil
+	}
+   	_, err = kv.Put(ctx, key, initValue)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// this will check if the rate limit's deadline has past. if it has
+// it will remove the limits, which will allow new requests to come in
+func (rl *RateLimiter) evaluateDeadline() (error) {
+    kv := clientv3.NewKV(rl.Etcd)
+	ctx:=context.Background()
+    gr, err := kv.Get(ctx, rl.keys["deadline"])
+	if err != nil {
+		return err
+	}
+	value:=string(gr.Kvs[0].Value)
+	deadline,err:=strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+	now := int(time.Now().Unix())
+	if deadline >= now {
+		kv.Delete(ctx, rl.BaseKey, clientv3.WithPrefix())
+	}
+	return nil
+}
+// Updates the current key, based on the base key
+func (rl *RateLimiter) incrementCurrentKey() (error) {
+	now := int(time.Now().Unix())
+	deadline:=int(time.Now().Local().Add(rl.Interval).Unix())
+	key:=rl.BaseKey
+	ctx:=context.Background()
+	ctx, _=context.WithCancel(ctx)
+    kv := clientv3.NewKV(rl.Etcd)
+
+	rl.initializeKeyIfNeeded(rl.keys["requests"], "0")
+	rl.initializeKeyIfNeeded(rl.keys["time_initialized"], strconv.Itoa(now))
+	rl.initializeKeyIfNeeded(rl.keys["deadline"], strconv.Itoa(deadline))
+
+    gr, err := kv.Get(ctx, rl.keys["requests"])
+	if err != nil {
+		return err
+	}
+	value:=string(gr.Kvs[0].Value)
+	requests,err:=strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+	requests=requests+1
+   	_, err = kv.Put(ctx, key, strconv.Itoa( requests ))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// IsOverLimit checks if we are over the limit we have set
+func (rl *RateLimiter) IsOverLimit() (bool,error) {
+	ctx:=context.Background()
+    kv := clientv3.NewKV(rl.Etcd)
+	gr,err:=kv.Get(ctx,rl.keys["requests"])
+	if err != nil {
+		return false,err
+	}
+	value:=string(gr.Kvs[0].Value)
+	requests,err:=strconv.Atoi(value)
+	if err != nil {
+		return false,err
+	}
+	if requests > int(rl.Limit) {
+		return true,nil
+	}
+	return false,nil
+}
+
+// Init starts the ticker, which takes care of periodically flushing/syncing the counter
+func (rl *RateLimiter) ProcessLimits() error {
+	if rl.Interval < time.Minute {
+		return fmt.Errorf("Minimum interval is 1 minute")
+	}
+	err:=rl.evaluateDeadline()
+	if err != nil {
+		return err
+	}
+
+	rl.incrementCurrentKey()
+	return nil
+}
+
+
+func main() {  
+	requestTimeout :=time.Duration(time.Second * 5)
+	dialTimeout:= time.Duration(time.Second * 5)
+
+    ctx, _ := context.WithTimeout(context.Background(), requestTimeout)
+    cli, _ := clientv3.New(clientv3.Config{
+        DialTimeout: dialTimeout,
+        Endpoints: []string{"127.0.0.1:2379"},
+    })
+    defer cli.Close()
+	key:="some_ip"
+	interval:=time.Duration(time.Second * 5)
+	flushinterval:=time.Duration(time.Second * 30)
+	var limit uint64 = 60
+	limiter := NewRateLimiter(cli, key,limit, interval, flushinterval)
+	limiter.ProcessLimits()
+
+
+	over,err:=limiter.IsOverLimit()
+	if err != nil {
+		return
+	}
+	if over {
+		fmt.Println("rate limit exhausted..")
+	}
+    kv := clientv3.NewKV(cli)
+    pr, _ := kv.Put(ctx, "key", "444")
+    rev := pr.Header.Revision
+    fmt.Println("Revision:", rev)
+    //gr, _ := kv.Get(ctx, "key")
+}
